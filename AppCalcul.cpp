8501d6b0c5b1e91ce439f386c357a72f79ab3667
vafagit <belgazzarwafae@gmail.com>
2021-11-12 17:22:29 +0100

    fCC 2

--- AppCalcul.cpp
+++ AppCalcul.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 	
-	int chx;
+	int chx1, chx2;
 	float x, y, rlt;
 	
 	printf("\t \t *******HELLO*******");
@@ -12,15 +12,15 @@ int main(){
 	printf("\t 3-Multiplication \n");
 	printf("\t 4-Division");
 	
-	printf("\t \n X = \n");
+	printf("\t \n X = \t");
 	scanf("\t \t %f", &x);
-	printf("\t enter voter choix \n");
-	scanf("\t \t %d", &chx);
-	printf("\t \n Y = \n");
+	printf("\t \n enter voter choix \t");
+	scanf("\t \t %d", &chx1);
+	printf("\t \n Y = \t");
 	scanf("\t \t %f", &y);
 	printf("\n \t \t");
 	 
-	switch (chx){
+	switch (chx1){
 		case 1 :
 			printf("RLT : %f \n", x + y);
 		break;
@@ -46,5 +46,46 @@ int main(){
 		
 		
 	}
-
+	int a, b;
+        
+        printf("oppe AND \n");
+        printf("enter le premier nomber \t \n ");
+        
+		printf("enter le deuxieme nomber \t \n");
+		scanf("%d %d", &a ,&b);
+		
+		if( (a== 0 || b==0)&&(a== 1  || b== 1);(a&&b == 1))
+		 {
+			printf("RLT = 1") ;
+		}
+		else if(a&&b == 1) {
+			printf("RLT = 0");
+		}
+	
+		else{
+			printf("choix invalide");
+		}
+		
+		
+			int a1, b1;
+        
+        printf("\n\t oppe AND \n");
+        printf("enter le premier nomber \t \n ");
+        
+		printf("enter le deuxieme nomber \t \n");
+		scanf("%d %d", &a1 ,&b1);
+		
+		if( (a1== 0 || b1==0)&&(a1== 1  || b1== 1);(a1||b1 == 1 ))
+		 {
+			printf("RLT = 1") ;
+		}
+		else if(a1||b1 == 0) {
+			printf("RLT = 1");
+		}
+	
+		else{
+			printf("choix invalide");
+		}
+		
+return 0 ;	
 }
