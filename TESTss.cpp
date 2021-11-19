ad87cb6fd512675b88e29de7a28b6da0319e1cc3
vafagit <belgazzarwafae@gmail.com>
2021-11-19 17:27:48 +0100

    v2

--- TESTss.cpp
+++ TESTss.cpp
@@ -4,62 +4,217 @@
 
 
 	
-	typedef struct PL {
-	  char nom [6];
-	  char prenom [6];
-	  char id[6]; 
-	};
+	typedef struct candidat  {
+	  char nom[12];
+	  char id; 
+	  int score;
+	} PL;
 	   	
-	typedef struct PE {
-	  char nom [10];
-	  char prenom [10];
-	  int id[10]; 
-	};
+	typedef struct electeur  {
+	char id; 
+	} EL;
+	
+	void vote1(electeur EL[], candidat PL[], int nb1, int nb2) {
+		int min, max;
+		double cal;
+		candidat PL1[nb1];
+		int chx, i,j,tour1,tour2, tour3,ep,x;
+		
+		do{
+		     i =0;
+			tour1 =0;
+			printf("\n");
+			printf("\n Tour 1");
+			printf("\n");
+			printf("\n electeur \t %s \t %d \t entrer votre choix ", PL1[i].id);
+			scanf("%d",&chx);
+			if(chx>0 && chx<=nb1){
+			 PL[chx-1].score++;
+			 i++;}
+			 else {
+			 	printf("choix invalide");
+			 }
+			}while(i <nb2);
+			
+			cal= nb2*0.15;
+			for(j=0;j<nb1;j++){
+				
+				if(PL1[j].score>=cal){
+					
+					PL1[tour1]= PL1[j];
+					tour1++;
+				}
+			}
+			
+			for(j=0; j<tour1;j++){
+				printf("les candidat selectioner pour le tour 1 : %s\t %d \n" ,PL[j].id ,PL[j].score);
+			}
+			
+			eq = PL1[0].score;
+			for(x= 0;x<nb1;x++){
+				if(eq != PL1[i].score){
+				
+				
+				}
+			}
+			
+			
+			
+	   void vote2(electeur EL[], candidat PL[], int nb1, int nb2) {
+		int min, max;
+		double cal;
+		candidat PL2[nb1];
+		int chx, i,j,tour1,tour2, tour3,ep,x;
+		
+		do{
+		     i =0;
+			tour1 =0;
+			printf("\n");
+			printf("\n Tour 1");
+			printf("\n");
+			printf("\n electeur \t %s \t %d \t entrer votre choix ", PL1[i].id);
+			scanf("%d",&chx);
+			if(chx>0 && chx<=nb1){
+			 PL[chx-1].score++;
+			 i++;}
+			 else {
+			 	printf("choix invalide");
+			 }
+			}while(i <nb2);
+			
+			cal= nb2*0.15;
+			for(j=0;j<nb1;j++){
+				
+				if(PL2[j].score>=cal){
+					
+					PL2[tour2]= PL2[j];
+					tour2++;
+				}
+			}
+			
+			for(j=0; j<tour2;j++){
+				printf("les candidat selectioner pour le tour 2 : %s\t %d \n" ,PL[j].id ,PL[j].score);
+			}
+			
+			eq = PL2[0].score;
+			for(x= 0;x<nb1;x++){
+				if(eq != PL2[i].score){
+				
+				
+				}
+			}
+			
+			
+			
+		}
+		
+	void vote3(electeur EL[], candidat PL[], int nb1, int nb2) {
+		int min, max;
+		double cal;
+		candidat PL3[nb1];
+		int chx, i,j,tour1,tour2, tour3,ep,x;
+		
+		do{
+		     i =0;
+			tour1 =0;
+			printf("\n");
+			printf("\n Tour 1");
+			printf("\n");
+			printf("\n electeur \t %s \t %d \t entrer votre choix ", PL1[i].id);
+			scanf("%d",&chx);
+			if(chx>0 && chx<=nb1){
+			 PL[chx-1].score++;
+			 i++;}
+			 else {
+			 	printf("choix invalide");
+			 }
+			}while(i <nb2);
+			
+			cal= nb2*0.15;
+			for(j=0;j<nb1;j++){
+				
+				if(PL3[j].score>=cal){
+					
+					PL3[tour3]= PL3[j];
+					tour3++;
+				}
+			}
+			
+			for(j=0; j<tour3;j++){
+				printf("les candidat selectioner pour le tour 3 : %s\t %d \n" ,PL[j].id ,PL[j].score);
+			}
+			
+			eq = PL3[0].score;
+			for(x= 0;x<nb1;x++){
+				if(eq != PL3[i].score){
+				
+				
+				}
+			}
+			
+			
+			
+		}
+	
+		
+	
+	
 
 int main(){
-	
- struct PL pl1;
- struct  PL pl2;
- struct PL pl3;
- struct PL pl4;
- struct PL pl5;
- struct PL pl6;
- 
-    strcpy(pl1.nom, "amrani");
-	strcpy(pl2.nom, "ezzarouti");
-	strcpy(pl3.nom, "benhima");
-	strcpy(pl4.nom, "brahimi");
-	strcpy(pl5.nom, "chouaref");
-	strcpy(pl6.nom, "benthami");
 
-	
-	
-	 strcpy(pl1.prenom, "ahmed");
-	strcpy(pl2.prenom, "mohamed");
-	strcpy(pl3.prenom, "selma");
-	strcpy(pl4.prenom, "marouan");
-	strcpy(pl5.prenom, "karim");
-	strcpy(pl6.prenom, "wassima");
-	
-	
-	strcpy(pl1.id, "A01");
-	strcpy(pl2.id, "B02");
-	strcpy(pl3.id, "D03");
-	strcpy(pl4.id, "S04");
-	strcpy(pl5.id, "F05");
-	strcpy(pl6.id, "Q06");
-	
-	int vote ;
+    int chx;
+    int x, y;
+    int nb1, nb2;
+	   
+	struct *PL;
+	struct *EL;
 	
 	printf("\t \t \t Election Presidentielle \t \t \n");
 	printf("\t \n ");
-    printPL( pl1 );
+	printf (" ");
+	printf("");
+ 
+	
+		do
+	{
+	  printf("enter nobre de liste :" );
+	  scanf("%d",&nb1);
+	}while(nb1<5);
+	
+	
+	
+	int *pl = (int*) malloc(nb1 *sizeof(int));
+	
+	for (int i = 0; i<nb1; i++){
+		pl[i]= i+1;
+	
+	    scanf("%d",&pl[i]);
+	    
+		}
+		for(int i=0; i<nb1;i++){
+		printf("pl[%d] \t %d\n",i,pl[i]);
+		}
 	
 	
 	
 	
+		do{
+	printf("enter nobre de liste :" );
+    scanf("%d",&nb2);
+	}while(nb2<10);
 	
 	
+	int *pe = (int*) malloc(nb2 *sizeof(int));
+	
+   	for (int i = 0; i<nb2; i++){
+		pl[i]= i+1;
+	    printf("enter id des electeurs [%d]:\n");
+      	scanf("%d",&pe[i]);
+		}
+	
+	for(int i=0; i<nb2;i++){
+			printf("pe[%d]  \t %d\n",i,pe[i]);
+		}
 	
 	
 	
